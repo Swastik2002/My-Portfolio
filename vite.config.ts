af6567d27788b8b3b7a5f0d58eb09a392a40b9f5
Swastik <swastikmanoj28@gmail.com>
2025-06-27 15:16:14 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import ghPages from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 const repoName = 'My-Portfolio';
 
